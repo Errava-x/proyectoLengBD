820f87d61fa8d4ca4565aea03e4e6c28979b1d65
ErickRamos <ramosvargaserickjose@gmail.com>
2026-04-17 22:48:17 -0600

    Push con organizacion de MVC

--- HappyPaws/src/main/java/module-info.java
+++ HappyPaws/src/main/java/module-info.java
@@ -7,4 +7,6 @@ module org.example.happypaws {
 
     opens org.example.happypaws to javafx.fxml;
     exports org.example.happypaws;
+    exports org.example.happypaws.Controllers;
+    opens org.example.happypaws.Controllers to javafx.fxml;
 }
